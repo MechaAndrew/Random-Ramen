ded2f7b93984fc0cab9ad6fc41aa431e072325f3
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-12-03 22:47:53 -0800

    WE FUCKIGN DID IT

--- js/site.js
+++ js/site.js
@@ -3,6 +3,43 @@
  *Created: 19 November 2021
  *License: Public Domain
  */
+
+//trial of css scaling on image thing
+var $el = $("#very-specific-design");
+var elHeight = $el.outerHeight();
+var elWidth = $el.outerWidth();
+
+var $wrapper = $("#scaleable-wrapper");
+
+$wrapper.resizable({
+  resize: doResize
+});
+
+function doResize(event, ui) {
+
+  var scale, origin;
+
+  scale = Math.min(
+    ui.size.width / elWidth,
+    ui.size.height / elHeight
+  );
+
+  $el.css({
+    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
+  });
+
+}
+
+var starterData = {
+  size: {
+    width: $wrapper.width(),
+    height: $wrapper.height()
+  }
+}
+doResize(null, starterData);
+
+
+
 //Functions:
 
 //Randomly choose which broth
@@ -50,10 +87,7 @@ function toppingNumber(){
     return 5;
   }  else if (mod == 6) {
     return 6;
-  }  else if (mod == 7) {
-    return 7;
   }
-
 }
   //randomly pick the topings
 function randomToppings(numbers){
