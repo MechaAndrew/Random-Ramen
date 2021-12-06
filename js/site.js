4b509ddde28bd8d4da2ca7c0961b63ed41d023e1
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-12-05 19:56:36 -0800

    background junk + other stuff

--- js/site.js
+++ js/site.js
@@ -3,22 +3,12 @@
  *Created: 19 November 2021
  *License: Public Domain
  */
-
-
 //Functions:
 
 //Randomly choose which broth
 function randomBroth(){
-  Math.floor(Math.random() * 2) + 1;
-  if (mod == 1) {
-
-  }
-  else if (mod == 2) {
-
-  }
-  else if (mod == 3) {
-
-  }
+  Math.floor(Math.random() * 3) + 1;
+$("#broth" + mod).css("visibility", "visible");
 }
 
 //randomly pick a ramen noodle type
@@ -26,20 +16,12 @@ function randomNoodle(){
 
   mod = Math.floor(Math.random() * (4-1)) + 1;
   console.log(Math.floor(Math.random() * (4-1)) + 1);
-  if (mod == 1) {
-$("#noodle1").css("visibility", "visible");
-  }
-  else if (mod == 2) {
-$("#noodle2").css("visibility", "visible");
-  }
-  else if (mod == 3) {
-$("#noodle3").css("visibility", "visible");
-  }
+$("#noodle" + mod).css("visibility", "visible");
 }//end randomNoodle
 
 //randomly pick the number of toppings
 function toppingNumber(){
-  mod = Math.floor(Math.random() *4) + 1;
+  mod = Math.floor(Math.random() *5) + 1;
   if (mod == 1) {
     return 1;
   }  else if (mod == 2) {
@@ -58,32 +40,16 @@ function toppingNumber(){
 function randomToppings(numbers){
 
   for(i=0; i<=numbers; i++) {
-  mod = Math.floor(Math.random() * (4-1)) + 1;
-  console.log(Math.floor(Math.random() * (4-1)) + 1);
+  mod = Math.floor(Math.random() * (29-1)) + 1;
+  console.log(Math.floor(Math.random() * (29-1)) + 1);
+  $("#topping" + mod).css("visibility", "visible");
   //implement a way so the toppings aren't repeated
-  if (mod == 1) {
-  $("#topping1").css("visibility", "visible");
-  }
-  else if (mod == 2) {
-  $("#topping2").css("visibility", "visible");
-  }
-  else if (mod == 3) {
-  $("#topping3").css("visibility", "visible");
-  }
-  else if (mod == 4){
-  $("#topping4").css("visibility", "visible");
-  }
-  else if (mod == 5){
-  $("#topping5").css("visibility", "visible");
-  }
-  else if (mod == 6){
-  $("#topping3").css("visibility", "visible");
-  }//end else if
 }//end for
 }//end Functions
 
 function randomRamen(){
   $(".noodle").css("visibility", "hidden");
+  $(".broth").css("visibility", "hidden");
   $(".topping").css("visibility", "hidden");
   // randomBroth();
 
@@ -92,6 +58,7 @@ function randomRamen(){
   // toppingNumber();
 
   randomToppings(toppingNumber());
+  randomBroth()
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -124,12 +91,12 @@ $("#random").click(function () {
 
 resetButton.click(function() {
 $(".noodle").css("visibility", "hidden");
+$(".broth").css("visibility", "hidden");
 $(".topping").css("visibility", "hidden");
 
 });
 //no need for a reset button. What else can we make the banner be?
 creditsButton.click(function() {});
 
-
-$("#history p").html("This shit bussin up my ass. fuck i hate this please someone just do thisi dont want to ode anymore. history shit gamin boyys. the fitner gram pacer test is a multi areobic penis. where my cock and balls are filled to the nrim but where at though coc kand ball torture to fill my cum of glaze")
+$("#history p").html("This shit bussin up my ass. history shit gamin boyys. the fitner gram pacer test is a multi areobic penis. where my cock and balls are filled to the nrim but where at though coc kand ball torture to fill my cum of glaze")
 // $("#health p").html("This shit bussin up my ass. history shit gamin boyys. the fitner gram pacer test is a multi areobic penis. where my cock and balls are filled to the nrim but where at though coc kand ball torture to fill my cum of glaze")
